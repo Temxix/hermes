40a2d77007f29a0054f4949b29f20e1066be2963
Temxix <tirrifogos@gmail.com>
2026-07-19 20:26:23 +0200

    Unset deprecated TERMINAL_CWD in run_bot.bat

--- run_bot.bat
+++ run_bot.bat
@@ -3,6 +3,9 @@ title Hermes Agent - Telegram Gateway
 echo Starting Hermes Agent Telegram Gateway...
 echo.
 
+set TERMINAL_CWD=
+set MESSAGING_CWD=
+
 cd /d "%~dp0hermes_source_code\hermes-agent-main"
 
 python -m gateway.run
